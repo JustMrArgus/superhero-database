bd5934f226ed47c58caf65581ef59b6e7e64a5c3
JustMrArgus <argusprogramming@protonmail.com>
2025-08-26 02:14:40 +0300

    Setup and configure react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import MainPage from "./pages/MainPage";
+import SuperheroPage from "./pages/SuperheroPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={MainPage} />
+          <Route path="/:id" element={SuperheroPage} />
+          <Route path="*" element={NotFoundPage} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
