1e3f4a38ed98236c8d55b2a5d7e0187ce4977e49
JustMrArgus <argusprogramming@protonmail.com>
2025-08-26 02:49:15 +0300

    Add footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-[#444444] text-white text-right pt-5 pb-1">
+      <p className="opacity-40">&copy; Oleksandr Rodina 2025</p>
+    </footer>
+  );
+};
+
+export default Footer;
