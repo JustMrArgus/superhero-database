9563fe8ecd0e9a6206c478cdf37b53bc985a2218
JustMrArgus <argusprogramming@protonmail.com>
2025-08-25 15:15:48 +0300

    Add server boot logic

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
 dotenv.config({ path: "./.env" });
+
+const app = require("./app");
+const connectDB = require("./db/connectDB");
+
+connectDB(process.env.DB).then(() => {
+  const port = process.env.port || 3000;
+  const server = app.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`);
+  });
+});
