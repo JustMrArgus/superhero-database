fb02eeb0211ededf1746865eeba69658c6a40696
JustMrArgus <argusprogramming@protonmail.com>
2025-08-25 16:48:55 +0300

    Addserver errors handler

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,18 @@ const app = express();
 
 const superheroesRoutes = require("./routes/superhero.routes");
 
+const globalErrorController = require("./controllers/error.controller");
+
 app.use(express.json({ limit: "10kb" }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/superheroes", superheroesRoutes);
+
+app.use((req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorController);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
 const dotenv = require("dotenv");
 
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception occured...");
+  console.log(err.name, err.message);
+  console.log(err);
+  process.exit(1);
+});
+
 dotenv.config({ path: "./.env" });
 
 const app = require("./app");
@@ -9,5 +16,11 @@ connectDB(process.env.DB).then(() => {
   const port = process.env.PORT || 3000;
   const server = app.listen(port, () => {
     console.log(`Server is listening on port ${port}...`);
+  });
+
+  process.on("unhandledRejection", (err) => {
+    console.log("Unhandled Rejection occured...");
+    console.log(err.name, err.message);
+    server.close(() => process.exit(1));
   });
 });
